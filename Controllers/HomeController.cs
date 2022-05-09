a34afdff84407fb86841464dcac3c95cad700717
hungtn <hungtn@tinhvan.com>
2022-05-09 21:50:27 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,297 @@
+﻿using ES_GUI.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ES_GUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private IHostingEnvironment _hostingEnvironment;
+
+        public HttpClient client;
+
+        HttpResponseMessage response;
+
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment hostingEnvironment)
+        {
+            _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44382/");
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Document(string index)
+        {
+            ViewBag.index = index;
+            return View();
+        }
+
+        public ContentResult GetIndexes()
+        {
+            try
+            {
+                response = client.GetAsync("GetIndexes").Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new ContentResult
+                    {
+                        Content = "{\"data\":" + "[]" + "}",
+                        ContentType = "application/json"
+                    };
+                }
+                var jsonResultAsString = response.Content.ReadAsStringAsync().Result;
+                return new ContentResult
+                {
+                    Content = jsonResultAsString,
+                    ContentType = "application/json"
+                };
+            }
+            catch
+            {
+                return new ContentResult
+                {
+                    Content = "{\"data\":" + "[]" + "}",
+                    ContentType = "application/json"
+                };
+            }
+        }
+
+        public ContentResult DeleteIndex(string index)
+        {
+            try
+            {
+                response = client.DeleteAsync("DeleteIndex/" + index).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new ContentResult
+                    {
+                        Content = "{\"data\":\"error\"}",
+                        ContentType = "application/json"
+                    };
+                }
+                var jsonResultAsString = response.Content.ReadAsStringAsync().Result;
+                return new ContentResult
+                {
+                    Content = jsonResultAsString,
+                    ContentType = "application/json"
+                };
+            }
+            catch
+            {
+                return new ContentResult
+                {
+                    Content = "{\"data\":\"error\"}",
+                    ContentType = "application/json"
+                };
+            }
+        }
+
+        public ContentResult CreateIndex(string index)
+        {
+            try
+            {
+                response = client.PostAsync("CreateIndex/" + index, null).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new ContentResult
+                    {
+                        Content = "{\"data\":\"error\"}",
+                        ContentType = "application/json"
+                    };
+                }
+                var jsonResultAsString = response.Content.ReadAsStringAsync().Result;
+                return new ContentResult
+                {
+                    Content = jsonResultAsString,
+                    ContentType = "application/json"
+                };
+            }
+            catch
+            {
+                return new ContentResult
+                {
+                    Content = "{\"data\":\"error\"}",
+                    ContentType = "application/json"
+                };
+            }
+        }
+
+        public ContentResult GetDocuments(string index)
+        {
+            try
+            {
+                response = client.GetAsync("GetDocuments/" + index).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new ContentResult
+                    {
+                        Content = "{\"data\":" + "[]" + "}",
+                        ContentType = "application/json"
+                    };
+                }
+                var jsonResultAsString = response.Content.ReadAsStringAsync().Result;
+                return new ContentResult
+                {
+                    Content = jsonResultAsString,
+                    ContentType = "application/json"
+                };
+            }
+            catch
+            {
+                return new ContentResult
+                {
+                    Content = "{\"data\":" + "[]" + "}",
+                    ContentType = "application/json"
+                };
+            }
+        }
+
+        public ContentResult AddDocuments(string index)
+        {
+            try
+            {
+                IFormFile file = Request.Form.Files[0];
+                string folderName = "UploadFile";
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                string newPath = Path.Combine(webRootPath, folderName);
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+                string fullPath = Path.Combine(newPath, file.FileName);
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                    stream.Position = 0;
+                }
+                using (StreamReader r = new StreamReader(fullPath))
+                {
+                    string arrayOfDocumentAsString = r.ReadToEnd();
+                    JArray jArray = JsonConvert.DeserializeObject<JArray>(arrayOfDocumentAsString);
+                    string jsonDocumentAsString;
+                    for (int i = 0; i < jArray.Count; i++)
+                    {
+                        jsonDocumentAsString = jArray[i].ToString();
+                        jsonDocumentAsString = jsonDocumentAsString.Replace(Environment.NewLine, string.Empty);
+                        response = client.PostAsync("AddDocument/" + index + "/" + jsonDocumentAsString, null).Result;
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            return new ContentResult
+                            {
+                                Content = "{\"data\":\"error\"}",
+                                ContentType = "application/json"
+                            };
+                        }
+                    }
+                }
+                var jsonReturnAsString = "{\"data\":\"success\"}";
+                return new ContentResult
+                {
+                    Content = jsonReturnAsString,
+                    ContentType = "application/json"
+                };
+            }
+            catch
+            {
+                return new ContentResult
+                {
+                    Content = "{\"data\":\"error\"}",
+                    ContentType = "application/json"
+                };
+            }
+        }
+
+        public ContentResult DeleteDocument(string index, string id)
+        {
+            try
+            {
+                response = client.DeleteAsync("DeleteDocument/" + index + "/" + id).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new ContentResult
+                    {
+                        Content = "{\"data\":\"error\"}",
+                        ContentType = "application/json"
+                    };
+                }
+                var jsonResultAsString = response.Content.ReadAsStringAsync().Result;
+                return new ContentResult
+                {
+                    Content = jsonResultAsString,
+                    ContentType = "application/json"
+                };
+            }
+            catch
+            {
+                return new ContentResult
+                {
+                    Content = "{\"data\":\"error\"}",
+                    ContentType = "application/json"
+                };
+            }
+        }
+
+        public ContentResult SearchDocuments(string index, string query)
+        {
+            try
+            {
+                response = client.GetAsync("SearchDocuments/" + index + "/" + query).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new ContentResult
+                    {
+                        Content = "{\"data\":" + "[]" + "}",
+                        ContentType = "application/json"
+                    };
+                }
+                var jsonResultAsString = response.Content.ReadAsStringAsync().Result;
+                return new ContentResult
+                {
+                    Content = jsonResultAsString,
+                    ContentType = "application/json"
+                };
+            }
+            catch
+            {
+                return new ContentResult
+                {
+                    Content = "{\"data\":" + "[]" + "}",
+                    ContentType = "application/json"
+                };
+            }
+        }
+    }
+}
---
+++ wwwroot/js/Custom.js
@@ -0,0 +1,230 @@
+﻿function GetIndexes(showAlert) {
+    $.ajax({
+        type: 'GET',
+        url: '/Home/GetIndexes',
+        data: {},
+        processData: false,
+        contentType: false
+    }).done(function (response) {
+        $('#query').val('');
+        var htmlString = '';
+        for (i = 0; i < response.data.length; i++) {
+            htmlString += '<tr>';
+            htmlString += '<td>' + response.data[i]["index"] + '</td>';
+            htmlString += '<td>' + response.data[i]["status"] + '</td>';
+            htmlString += '<td>' + response.data[i]["health"] + '</td>';
+            htmlString += '<td>' + response.data[i]["docs.count"] + '</td>';
+            htmlString += '<td>' + response.data[i]["store.size"] + '</td>';
+            htmlString += '<td>';
+            htmlString += '<button type="button" class="btn btn-primary" onclick="openDocumentPage(\'' + response.data[i]["index"] + '\')"><span class="glyphicon glyphicon-file"></span></button>';
+            htmlString += '&#160;&#160;';
+            htmlString += '<button type="button" class="btn btn-primary" onclick="showOpenFileDialog(\'' + response.data[i]["index"] + '\')"><span class="glyphicon glyphicon-open"></span></button>';
+            htmlString += '&#160;&#160;';
+            htmlString += '<button type="button" class="btn btn-danger" onclick="DeleteIndex(\'' + response.data[i]["index"] + '\')"><span class="glyphicon glyphicon-trash"></span></button>';
+            htmlString += '</td>';
+            htmlString += '</tr>';
+        }
+        $("#tbody-index").html(htmlString);
+        if (showAlert) {
+            $("#alert-success").fadeTo(2000, 500).slideUp(500, function () {
+                $("#alert-success").slideUp(2000);
+            });
+        }
+    });
+}
+
+function DeleteIndex(index) {
+    var formData = new FormData();
+    formData.append("index", index);
+    $.ajax({
+        type: 'POST',
+        url: '/Home/DeleteIndex',
+        data: formData,
+        processData: false,
+        contentType: false
+    }).done(function (response) {
+        if (response.data == "success") {
+            GetIndexes(true);
+        } else {
+            $("#alert-error").fadeTo(2000, 500).slideUp(500, function () {
+                $("#alert-error").slideUp(2000);
+            });
+        }
+    });
+}
+
+function CreateIndex(index) {
+    if (!index) {
+        return;
+    }
+    var formData = new FormData();
+    formData.append("index", index);
+    $.ajax({
+        type: 'POST',
+        url: '/Home/CreateIndex',
+        data: formData,
+        processData: false,
+        contentType: false
+    }).done(function (response) {
+        $('#index').val('');
+        $('#create-modal').modal('hide');
+        if (response.data == "success") {
+            GetIndexes(true);
+        } else {
+            $("#alert-error").fadeTo(2000, 500).slideUp(500, function () {
+                $("#alert-error").slideUp(2000);
+            });
+        }
+    });
+}
+
+function openDocumentPage(index) {
+    window.open('/Home/Document?index=' + index);
+}
+
+function GetDocuments(index, showAlert) {
+    var formData = new FormData();
+    formData.append("index", index);
+    $.ajax({
+        type: 'POST',
+        url: '/Home/GetDocuments',
+        data: formData,
+        processData: false,
+        contentType: false
+    }).done(function (response) {
+        if (response.data.length > 0) {
+            GenerateFieldTableDocument(response.data[0]);
+            GenerateRowTableDocument(response.data);
+        } else {
+            $("#thead-document").html('');
+            $("#tbody-document").html('');
+        }
+        if (showAlert) {
+            $("#alert-success").fadeTo(2000, 500).slideUp(500, function () {
+                $("#alert-success").slideUp(2000);
+            });
+        }
+    });
+}
+
+function showOpenFileDialog(index) {
+    $("#indexAddDocuments").val(index);
+    $('#addDocument-modal').modal('show');
+}
+
+function AddDocuments() {
+    if (!$("#indexAddDocuments").val()) {
+        return;
+    }
+    var fileExtension = ['json'];
+    var filename = $('#file').val();
+    if (filename.length == 0) {
+        return;
+    } else {
+        var extension = filename.replace(/^.*\./, '');
+        if ($.inArray(extension, fileExtension) == -1) {
+            return;
+        }
+    }
+    var formData = new FormData();
+    var fileUpload = $("#file").get(0);
+    var files = fileUpload.files;
+    formData.append(files[0].name, files[0]);
+    formData.append("index", $("#indexAddDocuments").val());
+    $.ajax({
+        type: "POST",
+        url: "/Home/AddDocuments",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("XSRF-TOKEN",
+                $('input:hidden[name="__RequestVerificationToken"]').val());
+        },
+        data: formData,
+        processData: false,
+        contentType: false
+    }).done(function (response) {
+        $('#file').val('');
+        $('#addDocument-modal').modal('hide');
+        if (response.data == "success") {
+            GetIndexes(true);
+        } else {
+            $("#alert-error").fadeTo(2000, 500).slideUp(500, function () {
+                $("#alert-error").slideUp(2000);
+            });
+        }
+    });
+}
+
+function DeleteDocument(index, id) {
+    var formData = new FormData();
+    formData.append("index", index);
+    formData.append("id", id);
+    $.ajax({
+        type: 'POST',
+        url: '/Home/DeleteDocument',
+        data: formData,
+        processData: false,
+        contentType: false
+    }).done(function (response) {
+        if (response.data == "success") {
+            GetDocuments(index, true);
+        } else {
+            $("#alert-error").fadeTo(2000, 500).slideUp(500, function () {
+                $("#alert-error").slideUp(2000);
+            });
+        }
+    });
+}
+
+function GenerateFieldTableDocument(document) {
+    var fields = Object.keys(document._source);
+    var htmlString = '';
+    htmlString += '<tr>';
+    for (i = 0; i < fields.length; i++) {
+        htmlString += '<th style="text-transform:uppercase">' + fields[i] + '</th>';
+    }
+    htmlString += '<th style="width:80px;text-transform:uppercase">Action</th>';
+    htmlString += '</tr>';
+    $("#thead-document").html(htmlString);
+}
+
+function GenerateRowTableDocument(documents) {
+    var fields = Object.keys(documents[0]._source);
+    var htmlString = '';
+    for (i = 0; i < documents.length; i++) {
+        var document = documents[i];
+        htmlString += '<tr>';
+        for (j = 0; j < fields.length; j++) {
+            htmlString += '<td>' + document._source[fields[j]] + '</td>';
+        }
+        htmlString += '<td>';
+        htmlString += '<button type="button" class="btn btn-danger" onclick="DeleteDocument(\'' + document._index + '\', \'' + document._id + '\')"><span class="glyphicon glyphicon-trash"></span></button>';
+        htmlString += '</td>';
+        htmlString += '</tr>';
+    }
+    $("#tbody-document").html(htmlString);
+}
+
+function SearchDocuments(index, query) {
+    if (!query) {
+        GetDocuments(index, false);
+    } else {
+        var formData = new FormData();
+        formData.append("index", index);
+        formData.append("query", query);
+        $.ajax({
+            type: 'POST',
+            url: '/Home/SearchDocuments',
+            data: formData,
+            processData: false,
+            contentType: false
+        }).done(function (response) {
+            if (response.data.length > 0) {
+                GenerateFieldTableDocument(response.data[0]);
+                GenerateRowTableDocument(response.data);
+            } else {
+                $("#thead-document").html('');
+                $("#tbody-document").html('');
+            }
+        });
+    }
+}
